00878e29d99f372f51090b9e24f2bb3cef1c5248
ToraEleazarTarigan-251401037 <toratarigan7d@gmail.com>
2026-06-08 14:23:38 +0700

    Menambah fitur

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -63,6 +63,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     //connect kan tombol button
     //percantik ui nya button alarm
+    //Tambah fitur
 
     ui->btnStartSW->setStyleSheet(gradientPrimaryStyle);
     ui->btnStartTimer->setStyleSheet(gradientPrimaryStyle);
